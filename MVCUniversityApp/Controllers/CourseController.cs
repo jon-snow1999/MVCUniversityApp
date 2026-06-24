c56a45f778c75fa0388f969acfd92f4ef4777e19
Abdulrahman Mostafa <Abdulrahmanmostafa5@gmail.com>
2026-06-24 12:08:19 +0300

    added a courses edit page

---
+++ MVCUniversityApp/Controllers/CourseController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using MVCUniversityApp.Context;
+using MVCUniversityApp.Models;
+using MVCUniversityApp.ViewModel;
+namespace MVCUniversityApp.Controllers
+{
+    public class CourseController : Controller
+    {
+
+        UniContext db = new UniContext();
+        public IActionResult Index()
+        {
+            
+            return View("all", db.Courses.ToList());
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Course courseFromDB = db.Courses.FirstOrDefault((course) => course.Id == id);
+            CourseWithDeptList cd = new CourseWithDeptList
+            {
+                course = courseFromDB,
+                departments = db.Departments.ToList()
+            };
+            if(courseFromDB != null)
+            {
+                return View("edit", cd);
+            }
+            return View("edit");
+        }
+
+        [HttpPost]
+        public IActionResult editCourse(Course courseFromForm)
+        {
+
+            //Course corseFromDB = db.Courses.FirstOrDefault((course) => course.Id == courseFromForm.Id);
+            if(courseFromForm.Name != null && courseFromForm.MaxDegree != null && courseFromForm.MinDegree != null)
+            {
+                db.Courses.Update(courseFromForm);
+                db.SaveChanges();
+                return RedirectToAction(actionName: "Index", controllerName: "Course");
+            } else
+            {
+                CourseWithDeptList cd = new CourseWithDeptList
+                {
+                    course = courseFromForm,
+                    departments = db.Departments.ToList()
+                };
+                return View("edit", cd);
+            }
+            
+        }
+    }
+}
---
+++ MVCUniversityApp/ViewModel/CourseWithDeptList.cs
@@ -0,0 +1,10 @@
+﻿using MVCUniversityApp.Models;
+
+namespace MVCUniversityApp.ViewModel
+{
+    public class CourseWithDeptList
+    {
+        public Course course { get; set; }
+        public List<Department> departments { get; set; }
+    }
+}
